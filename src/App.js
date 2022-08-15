965b2ac7b4b7fd8550aa558c64c2d8c30fb263c3
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-08-15 16:02:55 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import ScrollToTop from './components/layouts/ScrollToTop';
+import ScrollToTop from "./components/ScrollToTop";
 
-import NavBar from './components/layouts/NavBar';
-import Footer from './components/layouts/Footer';
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import Contact from './components/pages/Contact';
-
-import './styles/App.css';
-import './styles/Home.css';
-import './styles/Contact.css';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
 
   return (
-    
+
     <div>
 
       <Router>
 
         <ScrollToTop />
 
-        <NavBar/>
+        <NavBar />
 
         <Routes>
 
-          <Route path='/' element={<Home/>} />
+          <Route path="/" element={<Home />} />
 
-          <Route path='/sobre' element={<About/>} />
+          <Route path="/sobre" element={<About />} />
 
-          <Route path='/contato' element={<Contact/>} />
+          <Route path="/contato" element={<Contact />} />
 
         </Routes>
 
-        <Footer/>
+        <Footer />
 
       </Router>
 
