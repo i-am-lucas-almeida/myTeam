8e74456aa91a3e7f0faf88a815db30773ac8e631
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-08-15 16:07:07 -0300

    Create pages folder

---
+++ src/pages/About.js
@@ -0,0 +1,47 @@
+import "../styles/pages/About.css";
+
+import PreFooter from "../components/PreFooter";
+import Clients from "../components/Clients";
+import Profile from "../components/Profile";
+
+import imagePattern_1 from "../images/bg-pattern-about-1-mobile-nav-1.svg";
+
+export default function About() {
+
+    return (
+
+        <div>
+
+            <div className="container">
+
+                <div className="about-top">
+
+                    <div className="about-top_left animate__animated animate__fadeInUp">
+
+                        <h1>Quem somos?</h1>
+
+                    </div>
+
+                    <div className="about-top_right animate__animated animate__fadeInUp">
+
+                        <p>Ajudamos as empresas a construir equipes dinâmicas compostas pelos melhores talentos globais. Usando nossa rede de profissionais apaixonados, impulsionamos a inovação e entregamos resultados incríveis. Equipes talentosas e diversificadas moldam os melhores produtos e experiências. Levaremos essas equipes até você.</p>
+
+                    </div>
+
+                    <img src={imagePattern_1} alt="imagem de fundo" className="pattern-about_right" />
+
+                </div>
+
+            </div>
+
+            <Profile />
+
+            <Clients />
+
+            <PreFooter />
+
+        </div>
+
+    );
+
+}
---
+++ src/pages/Contact.js
@@ -0,0 +1,68 @@
+import "../styles/pages/Contact.css";
+
+import imageContact_1 from "../images/icon-person.svg";
+import imageContact_2 from "../images/icon-cog.svg";
+import imageContact_3 from "../images/icon-chart.svg";
+
+import imagePattern_1 from "../images/bg-pattern-home-1.svg";
+import imagePattern_2 from "../images/bg-pattern-contact-2.svg";
+
+import Form from "../components/Form";
+
+export default function Contact() {
+
+    return (
+
+        <div className="container">
+
+            <img src={imagePattern_1} alt="imagem de fundo" className="pattern-contact_top" />
+
+            <div className="contact-c">
+
+                <div className="box-contact_left animate__animated animate__fadeInUp">
+
+                    <h1>Contato</h1>
+
+                    <h2>Pergunte-nos sobre</h2>
+
+                    <div className="box-contact_about">
+
+                        <aside>
+
+                            <img src={imageContact_1} alt="imagem qualidade dos talentos" />
+
+                            <p>A qualidade da nossa rede de talentos</p>
+
+                        </aside>
+
+                        <aside>
+
+                            <img src={imageContact_2} alt="imagem ferramentas do software" />
+
+                            <p>Uso e implementação do nosso software</p>
+
+                        </aside>
+
+                        <aside>
+
+                            <img src={imageContact_3} alt="imagem impulsionar a inovação" />
+
+                            <p>Como ajudamos a impulsionar a inovação</p>
+
+                        </aside>
+
+                    </div>
+
+                </div>
+
+                <Form />
+
+            </div>
+
+            <img src={imagePattern_2} alt="imagem de fundo" className="pattern-contact_bottom" />
+
+        </div>
+
+    );
+
+}
